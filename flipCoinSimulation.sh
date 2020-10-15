5c374eb90d0a0069495cb67038f86ec0ac1c5f85
“Enter <shaikhfaisal6342@gmail.com>
2020-10-15 13:57:22 +0530

    singlet combination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,29 @@
 #!/bin/bash -x
-
+declare -A result
 flipCoinSimulation(){
-a=$((RANDOM%2))
-if [[ $a -eq 1 ]]
-then
-	echo "Head"
-else
-	echo "tail"
-fi
+head=0;
+tail=0;
+
+#using loop uc2
+
+read -p "Enter a number for flip coin :" n
+
+for (( i=0; i<n; i++ ))
+{
+	a=$((RANDOM%2));
+
+	if [[ a -eq 1 ]]
+		then
+			head=$((head+1))
+		else
+			tail=$((tail+1))
+	fi
+}
+echo "Number of head win "$head"
+echo "Number of tail win "$tail"
+
+result[H]="$head"
+result[T]="$tail"
+echo "Dictionary : " ${result[@]}
 }
 flipCoinSimulation
