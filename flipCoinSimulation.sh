122d4233c123eea5a516c9163fd14f6e6489d5d0
“Enter <shaikhfaisal6342@gmail.com>
2020-10-15 14:08:13 +0530

    Doublet combination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -18,12 +18,39 @@ for (( i=0; i<n; i++ ))
 		else
 			tail=$((tail+1))
 	fi
+	
+	d=$(( RANDOM%4 ))
+	case $d in
+		0)
+			 hh=$(($hh+1))
+			;;
+		1) 
+			ht=$(($ht+1))
+			;;
+		2)
+			th=$(($th+1))
+			;;
+		3)
+			tt=$(($tt+1))
+			;;
+		*)
+			echo "Went Wrong"
+			;;
+	esac
+
 }
 echo "Number of head win "$head"
 echo "Number of tail win "$tail"
-
 result[H]="$head"
 result[T]="$tail"
 echo "Dictionary : " ${result[@]}
+
+
+echo "number of hh combination " $hh
+echo "number of ht combination " $ht
+echo "number of th combination " $th
+echo "number of tt combination " $tt
+
+
 }
 flipCoinSimulation
